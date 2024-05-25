81b5c4a614ecaf11e1ca8d679fa1534c514a6676
KartiKBriteinno <153173760+KartiKBriteinno@users.noreply.github.com>
2024-05-25 16:38:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -57,3 +57,43 @@ progress.addEventListener('mousedown', () => mousedown = true);
 progress.addEventListener('mouseup', () => mousedown = false);
 
 skipButtons.forEach(button => button.addEventListener('click', skip));
+
+
+
+describe('Custom Video Player', () => {
+  it('should load the video player and control elements', () => {
+    cy.visit('index.html');
+    cy.get('.viewer').should('have.prop', 'paused', true);
+    cy.get('.toggle').should('contain', '►');
+  });
+
+  it('should play and pause the video', () => {
+    cy.get('.toggle').click();
+    cy.get('.viewer').should('have.prop', 'paused', false);
+    cy.get('.toggle').should('contain', '❚ ❚');
+    cy.get('.toggle').click();
+    cy.get('.viewer').should('have.prop', 'paused', true);
+    cy.get('.toggle').should('contain', '►');
+  });
+
+  it('should change the volume and playback rate', () => {
+    cy.get('[name="volume"]').invoke('val', 0.5).trigger('change');
+    cy.get('.viewer').should('have.prop', 'volume', 0.5);
+    cy.get('[name="playbackRate"]').invoke('val', 1.5).trigger('change');
+    cy.get('.viewer').should('have.prop', 'playbackRate', 1.5);
+  });
+
+  it('should skip forward and backward', () => {
+    cy.get('.viewer').invoke('prop', 'currentTime', 50);
+    cy.get('[data-skip="-10"]').click();
+    cy.get('.viewer').should('have.prop', 'currentTime', 40);
+    cy.get('[data-skip="25"]').click();
+    cy.get('.viewer').should('have.prop', 'currentTime', 65);
+  });
+
+  it('should update the progress bar as the video plays', () => {
+    cy.get('.viewer').invoke('prop', 'currentTime', 50);
+    cy.get('.viewer').invoke('prop', 'duration', 100);
+    cy.get('.progress__filled').should('have.css', 'flex-basis', '50%');
+  });
+});
